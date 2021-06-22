37354cd40ecbb5978056d91e4f91fdb290424d44
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-06-22 20:04:22 +0530

    update

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,5 +1,28 @@
-let box = document.querySelectorAll(".box");
+let firstBoxes = document.querySelectorAll(".first li");
 
-console.log(box);
+firstBoxes.forEach((box, index) =>{
+    box.addEventListener("click" , function(event){
+        event.target.innerText = index + 1;
+
+        setTimeout(()=>{
+event.target.innerText= "";
+        } ,5000)
+    })
+})
 
 
+let secondBox = document.querySelector(".second");
+
+secondBox.addEventListener("click" , function(event){
+  
+    let text = event.target.dataset.text;
+ 
+    event.target.innerText = text;
+
+
+    setTimeout(()=>{
+        event.target.innerText= "";
+                } ,5000)
+  
+
+})
