06ce8abf5283bfc70f0c144718a4d946db65ad94
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-06-22 18:11:06 +0530

    update

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -41,3 +41,95 @@ function generaterRandomColor(){
    }
 
    parentBox.addEventListener("mousemove" , handleMouseMove);
+
+
+
+/*function main(){
+    let inc = document.querySelector(".inc");
+
+    let count = 0;
+ 
+    let counter = document.querySelector('h1')
+ 
+    inc.addEventListener("click" , function(){
+ 
+        count = count + 1;
+ 
+         counter.innerText = count ;
+    })
+ 
+ 
+ 
+    let decrement = document.querySelector(".decrement");
+ 
+    decrement.addEventListener("click" , function(){
+ 
+     count = count - 1
+        counter.innerText = count ;
+    }
+    )
+ 
+    let reset = document.querySelector(".reset");
+ 
+    reset.addEventListener("click" , function(){
+        count = 0;
+        counter.innerText = count ;
+    })
+ 
+ 
+ 
+ counter.innerText = count ;
+}
+
+main(); */
+
+
+/*let COLORS = [
+        "Red", 
+        "Orange ",
+        "Yellow ",
+        "Green ",
+        "Cyan ",
+        "Blue ",
+        "Purple ",
+        "Pink ",
+        "White ",
+        "Gray ",
+        "Brown ",
+]
+
+let root = document.querySelector(".buttons")
+
+let h1 = document.querySelector("h1");
+
+function handleClick( colorText , e){
+    if( e.shiftKey === true){
+        h1.innerText = colorText;
+    h1.style.backgroundColor = colorText;
+
+    } else{
+        h1.innerText = "Use shift key to change Color";
+    }
+    
+   }
+
+
+COLORS.forEach((color) =>{
+
+    let div = document.createElement("div");
+
+  
+
+    div.classList.add("box");
+
+    div.addEventListener("click", function(event){
+        handleClick(color , event);
+    })
+
+
+        div.style.backgroundColor = color;
+
+        root.append(div);
+})
+
+*/
